8955840dacc6d34012f2b8a2bac43d950136130a
MiSinG <1608839567@qq.com>
2023-08-31 20:29:06 +0800

    feat:单向链表

---
+++ data_structure-algorithm/src/main/java/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+/**
+ * @Description: TODO  普通单向链表
+ * @Author MiSinG
+ * @Date 2023/8/31
+ * @Version V1.0
+ **/
+public class SinglyLinkedList {
+    int data;
+    SinglyLinkedList next;
+
+    public SinglyLinkedList(int data, SinglyLinkedList next) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
