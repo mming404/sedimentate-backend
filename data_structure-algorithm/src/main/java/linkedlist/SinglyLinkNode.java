298e8c97d583633a643fc16895ba4a1ad34a02ad
MiSinG <1608839567@qq.com>
2023-09-01 13:57:16 +0800

    feat:单向链表插入节点

---
+++ data_structure-algorithm/src/main/java/linkedlist/SinglyLinkNode.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+/**
+ * @Description: TODO  普通单向链表
+ * @Author MiSinG
+ * @Date 2023/8/31
+ * @Version V1.0
+ **/
+public class SinglyLinkNode {
+    int data;
+    SinglyLinkNode next;
+
+    public SinglyLinkNode(int data, SinglyLinkNode next) {
+        this.data = data;
+        this.next = next;
+    }
+}
--- data_structure-algorithm/src/main/java/linkedlist/SinglyLinkedList.java
+++ data_structure-algorithm/src/main/java/linkedlist/SinglyLinkedList.java
@@ -1,18 +1,83 @@
 package linkedlist;
 
+import org.yaml.snakeyaml.events.Event;
+
 /**
- * @Description: TODO  普通单向链表
+ * @Description: TODO
  * @Author MiSinG
- * @Date 2023/8/31
+ * @Date 2023/9/1
  * @Version V1.0
  **/
 public class SinglyLinkedList {
-    int data;
-    SinglyLinkedList next;
+    private SinglyLinkNode head;// 头节点。
+    private SinglyLinkNode tail;// 尾节点。
+    private int size;// 链表的长度。
 
-    public SinglyLinkedList(int data, SinglyLinkedList next) {
-        this.data = data;
-        this.next = next;
+    //尾部添加
+    public void add(int data){
+        insert(size,data);
     }
 
+    //删除头节点
+//    public int poll(){
+//
+//    }
+
+    //插入节点
+    public void insert(int index,int data){
+        //如果是头节点
+        if (index == 0){
+            head  = new SinglyLinkNode(data,head);
+            if (size == 0){
+                tail = head;
+            }
+        }else if (index == size){//在末尾加
+            tail.next= new SinglyLinkNode(data,null);
+            tail = tail.next;
+        }else {//在中间加
+            //找出前一个节点
+            SinglyLinkNode linkNode = findPre(index);
+            //将前一个节点的next给新节点的next 同时前一个节点的next指向新节点
+            linkNode.next = new SinglyLinkNode(data,linkNode.next);
+        }
+        size++;
+    }
+
+    /**
+     * 查找index前一个节点
+     */
+    SinglyLinkNode findPre(int index){
+        // index 必须是有效的，不能越界。查找前一个，index可以等于size。
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("越界了……");
+        }
+        if (index == 0)// 头节点的前一个是空节点。
+        {
+            return null;
+        }
+        SinglyLinkNode preNode = head;
+        for (int i = 0; i < index - 1; i++) {
+            preNode = preNode.next;
+        }
+        return preNode;
+    }
+
+    /**
+     * 查找index后一个节点
+     */
+    SinglyLinkNode findNext(int index){
+        // index 必须是有效的，不能越界。查找前一个，index可以等于size。
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("越界了……");
+        }
+        if (index == 0)// 头节点的前一个是空节点。
+        {
+            return null;
+        }
+        SinglyLinkNode preNode = head;
+        for (int i = 0; i < index - 1; i++) {
+            preNode = preNode.next;
+        }
+        return preNode;
+    }
 }
