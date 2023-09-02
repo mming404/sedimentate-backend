661e59bc56206b0d70450139b60a470efb397caa
MiSinG <1608839567@qq.com>
2023-09-02 14:17:31 +0800

    feat:双向链表测试

--- data_structure-algorithm/src/main/java/www/Test.java
+++ data_structure-algorithm/src/main/java/www/Test.java
@@ -1,5 +1,8 @@
 package www;
 
+import linkedlist.DoubleLinkedList;
+import linkedlist.SinglyLinkedList;
+
 /**
  * @Description: TODO
  * @Author MiSinG
@@ -9,6 +12,23 @@ package www;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello world!!");
+//        System.out.println("hello world!!");
+//        SinglyLinkedList linkedList = new SinglyLinkedList();
+//        linkedList.add(1);
+//        linkedList.add(2);
+//        linkedList.add(3);
+//        linkedList.insert(1,8);
+//        linkedList.remove(1);
+//        System.out.println(linkedList.size());
+
+        DoubleLinkedList linkedList = new DoubleLinkedList();
+        linkedList.addEnd(1);
+        linkedList.addEnd(2);
+        linkedList.addEnd(3);
+        linkedList.addEnd(4);
+        linkedList.addHead(0);
+        System.out.println(linkedList.findNode(3).data);
+
     }
+
 }
