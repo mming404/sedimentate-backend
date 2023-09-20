74d5d752adbeb3b6e227a2ed7a1a223e826bc100
MiSinG <1608839567@qq.com>
2023-09-20 12:59:06 +0800

    test

---
+++ data_structure-algorithm/src/main/java/ds/table/HashMapTest.java
@@ -0,0 +1,26 @@
+package ds.table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description: TODO
+ * @Author MiSinG
+ * @Date 2023/9/20
+ * @Version V1.0
+ **/
+public class HashMapTest {
+    public void tsethash(){
+        HashMap<Integer, String> map = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+
+        }
+
+        for (Integer i : map.keySet()) {
+
+        }
+
+        System.out.println("dawd".hashCode());
+
+    }
+}
